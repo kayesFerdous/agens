e965b51a150a1d9fe194e2f105dbce643ef8f90c
kayesFerdous <kayesfardows@gmail.com>
2026-05-13 11:08:25 +0600

    ui: update ChatArea layout to center content with flexbox

--- frontend/src/components/ChatArea.svelte
+++ frontend/src/components/ChatArea.svelte
@@ -633,6 +633,8 @@
     left: 0;
     right: 0;
     display: flex;
+    flex-direction: column;
+    align-items: center;
     justify-content: center;
     padding-bottom: 16px;
     padding-top: 24px;
